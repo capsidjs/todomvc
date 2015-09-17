a409fa4e678d891441ec5beadd8143db0c4b889a
Yoshiya Hinosawa <stibium121@gmail.com>
2015-09-17 16:15:00 +0900

    Update todo-app

--- src/service/todo-app.js
+++ src/service/todo-app.js
@@ -9,11 +9,41 @@ var TodoApp = $.cc.subclass(function (pt, parent) {
 
         this.elem = elem;
 
+        this.initEvents();
+
+    };
+
+
+    pt.initEvents = function () {
+
+        var that = this;
+
         this.elem.on('todo-new-item', function (e, item) {
 
             console.log('todo-new-item: ' + item);
 
+            that.addTodo(item);
+
         });
+
+    };
+
+    pt.addTodo = function (todoBody) {
+
+        var todo = todoFactory.createByBody(todoBody);
+
+        this.todoList.push(todo);
+
+        this.updateTodoList();
+
+    };
+
+    /**
+     * Updates the todo list by the current state.
+     */
+    pt.updateTodoList = function () {
+
+        this.elem.find('.todo-list').cc.get('todo-list').update(todoList);
 
     };
 
