3c018e94439cb94abb36212d4ff16482921cd5bf
Yoshiya Hinosawa <stibium121@gmail.com>
2015-09-21 01:02:54 +0900

    Add coverage settings

--- karma.conf.js
+++ karma.conf.js
@@ -35,11 +35,16 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
+
+    coverageReporter: {
+        type: 'lcov'
+    },
 
 
     browserify: {
-        debug: true
+        debug: true,
+        transform: [require('browserify-istanbul')({ignore: '**/spec/**/*'})]
     },
 
 
--- spec/helper.js
+++ spec/helper.js
@@ -1,2 +1,1 @@
-global.$ = global.jQuery = require('jquery');
-require('class-component');
+require('../src');
