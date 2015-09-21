c4ad6e9bb51383a651e9a6cf6bf3f5f0fa1f05a8
Yoshiya Hinosawa <stibium121@gmail.com>
2015-09-22 00:32:52 +0900

    Add test of todo-clear-btn

---
+++ spec/components/todo-clear-btn.js
@@ -0,0 +1,18 @@
+
+
+describe('TodoClearBtn', function () {
+    'use strict';
+
+    it('triggers todo-clear-completed event when clicked', function (done) {
+
+        var dom = $('<button />');
+
+        dom.cc.init('todo-clear-btn');
+
+        dom.on('todo-clear-completed', function () { done(); });
+
+        dom.click();
+
+    });
+
+});
