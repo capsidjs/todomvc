d52c419e8137cc1f368f0b81e0db6df6c571da97
Yoshiya Hinosawa <stibium121@gmail.com>
2015-10-05 12:17:32 +0900

    Add test of todo-list

---
+++ spec/components/todo-list.js
@@ -0,0 +1,34 @@
+/* global describe, it, expect, beforeEach */
+
+var TodoCollection = require('../../src/domain/todo-collection');
+var TodoFactory = require('../../src/domain/todo-factory');
+todoFactory = new TodoFactory();
+
+var todoList;
+
+describe('todo-list', function () {
+	'use strict';
+
+	beforeEach(function () {
+
+		todoList = $('<div />').cc.init('todo-list');
+
+	});
+
+	describe('update', function () {
+
+		it('updates the todo list view by the given todo array', function () {
+
+			todoList.update(new TodoCollection([
+				todoFactory.createByTitle('foo0'),
+				todoFactory.createByTitle('foo1'),
+				todoFactory.createByTitle('foo2')
+			]));
+
+			expect(todoList.elem.find('li')).to.have.length(3);
+
+		});
+
+	});
+
+});
