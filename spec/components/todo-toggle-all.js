04757ebad6e40cc582dcf1fe4465b84a6940a7b5
Yoshiya Hinosawa <stibium121@gmail.com>
2015-10-06 14:51:15 +0900

    Add placeholder of spec of todo-toggle-all

---
+++ spec/components/todo-toggle-all.js
@@ -0,0 +1,14 @@
+/* global describe, it, expect, beforeEach */
+
+
+describe('todo-toggle-all', function () {
+
+	beforeEach(function () {});
+
+	describe('toggleAll', function () {
+	});
+
+	describe('updateBtnState', function () {
+	});
+
+});
