167135173fd72b46fb899765506afbccb39111af
Yoshiya Hinosawa <stibium121@gmail.com>
2016-06-05 16:05:58 +0900

    feat(filter): add filter model

---
+++ src/domain/filter.js
@@ -0,0 +1,19 @@
+const Const = require('../const')
+
+class Filter {
+  /**
+   */
+  constructor(name, isAll) {
+    this.name = name
+  }
+
+  isAll() {
+    return this.name === Const.FILTER.ALL
+  }
+}
+
+Filter.ALL = new Filter(Const.FILTER.ALL)
+Filter.ACTIVE = new Filter(Const.FILTER.ACTIVE)
+Filter.COMPLETED = new Filter(Const.FILTER.COMPLETED)
+
+module.exports = Filter
