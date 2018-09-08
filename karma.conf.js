f0a41df65f6a78f32384ba6d2456049a80b5acea
Yoshiya Hinosawa <stibium121@gmail.com>
2018-09-08 13:18:03 +0900

    chore: update to babel 7

--- karma.conf.js
+++ karma.conf.js
@@ -1,15 +1,25 @@
-module.exports = config => config.set({
-  frameworks: ['mocha', 'browserify'],
-  files: ['src{/,/**/}__tests__/*.js'],
-  preprocessors: { 'src/**/*': 'browserify' },
-  reporters: ['progress', 'coverage'],
-  coverageReporter: {type: 'lcov'},
-  browserify: {
-    debug: true,
-    transform: [
-      ['babelify', { presets: ['es2015', 'decorators-legacy'], plugins: ['istanbul'] }]
-    ]
-  },
-  browsers: ['Chrome', 'Firefox'],
-  singleRun: true
-})
+module.exports = config =>
+  config.set({
+    frameworks: ['mocha', 'browserify'],
+    files: ['src{/,/**/}__tests__/*.js'],
+    preprocessors: { 'src/**/*': 'browserify' },
+    reporters: ['progress', 'coverage'],
+    coverageReporter: { type: 'lcov' },
+    browserify: {
+      debug: true,
+      transform: [
+        [
+          'babelify',
+          {
+            presets: ['@babel/preset-env'],
+            plugins: [
+              ['@babel/plugin-proposal-decorators', { legacy: true }],
+              'istanbul'
+            ]
+          }
+        ]
+      ]
+    },
+    browsers: ['Chrome', 'Firefox'],
+    singleRun: true
+  })
